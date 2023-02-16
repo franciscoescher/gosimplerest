006b49f6a6ed9db3d7c3b73802e97be7bfe2ecf4
Francisco Escher <egs.francisco@gmail.com>
2023-02-16 00:43:33 -0300

    minor

--- resource/find.go
+++ resource/find.go
@@ -10,7 +10,8 @@ import (
 func (b *Resource) Find(base *Base, id any) (map[string]any, error) {
 	fields := b.GetFieldNames()
 
-	response := base.DB.QueryRow(fmt.Sprintf(`SELECT %s FROM %s WHERE %s = ? LIMIT 1`, strings.Join(fields, ","), b.Table, b.PrimaryKey), id)
+	sqlStatement := fmt.Sprintf(`SELECT %s FROM %s WHERE %s = ? LIMIT 1`, strings.Join(fields, ",")
+	response := base.DB.QueryRow(sqlStatement, b.Table, b.PrimaryKey), id)
 
 	values := make([]any, len(b.Fields))
 	scanArgs := make([]any, len(b.Fields))
